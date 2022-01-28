85588ddc0cbf7735adb52af3b7752c88f1b93d14
Christian <christianfernandez.alu@iespacomolla.es>
2022-01-28 16:33:57 +0100

    terminado

--- recoger_acta.php
+++ recoger_acta.php
@@ -3,14 +3,11 @@
 require_once "bd.php";
 
 if(isset($_POST["formulario"])){
-    mkdir("./acta", 0700);
     $archivo = $_FILES["archivo"]["name"];
     $archivo_tmp = $_FILES["archivo"]["tmp_name"];
     $ruta= './acta/'.$archivo;
     $rutaCarpeta='./acta';
     move_uploaded_file($archivo_tmp, $ruta);
-    $base_dir = realpath($_SERVER["DOCUMENT_ROOT"]);
-    $file_delete =  "$base_dir\acta";
 
     $filas = file($ruta);
     foreach ($filas as $value) {
@@ -36,7 +33,7 @@ if(isset($_POST["formulario"])){
 
 
     }
-
+    unlink($ruta);
 
 }
 ?>
